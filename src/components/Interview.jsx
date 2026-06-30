e9e10ec89200e5232c02440ddc6012ceea46ef72
CoderZENNXX <brocodestudent@gmail.com>
2026-06-30 14:03:23 +0700

    Final V (30/6/26)

--- src/components/Interview.jsx
+++ src/components/Interview.jsx
@@ -22,7 +22,7 @@ const interviews = [
     interviewer: "Interviewed by Hsu Won Yan Moe",
     opinion: "Hsu's Opinion",
     opinionText:
-      "In my opinion, the interviewee has a lot of nuanced takes and I tend to agree with almost most of them. I acknowledge the fact China has had very much of an impact on Burma, our country. I believe they have controlled fields such as most of the Burmese government, business, economics and education.",
+      "From my perspective, I agree with what she says. I believe that smaller countries surrounding these two powerful nations can be significantly affected, particularly in terms of technology, security, and trade. Lastly, the way each country is governed and managed could also change considerably as a result of this rivalry.",
   },
   {
     name: "Saw Steven Htoo",
@@ -31,7 +31,7 @@ const interviews = [
     interviewer: "Interviewed by Eh Mu Thaw Htoo Phoe",
     opinion: "Eh Mu's Opinion",
     opinionText:
-      "In my opinion, the interviewee has a lot of nuanced takes and I tend to agree with almost most of them. I acknowledge the fact China has had very much of an impact on Burma, our country. I believe they have controlled fields such as most of the Burmese government, business, economics and education.",
+      "The rivalry affected the world by increasing trade tensions, competition in technology, and political disagreements. Personally, I noticed the higher prices for some products and conflicts between the two countries. It also made me realize how decisions made by powerful countries can affect people’s daily lives aroumd the world.",
   },
 ];
 
